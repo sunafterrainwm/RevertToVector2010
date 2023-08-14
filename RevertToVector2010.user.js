7735d3166ca7ea70e99760e8da9b14971dc69759
sunafterrainwm <sunafterrainwm@gmail.com>
2023-08-14 21:44:55 +0800

    Update RevertToVector2010.user.js

--- RevertToVector2010.user.js
+++ RevertToVector2010.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Revert To Vector 2010
-// @version      0.0.1+2023081001
+// @version      0.0.1+2023081401
 // @author       sunafterrainwm
 // @description  Rewrite URLs to force legacy Vector skin when user is not logged in.
 // @description:zh  在用户未登入 WMF wikis 时重写网址以强制使用旧版 Vector 皮肤。
@@ -45,7 +45,7 @@
             || window.location.pathname === '/w/api.php'
             || mw.config.get('skin') !== 'vector-2022'
             || window.location.search.match(/[?&]useskin=vector-2022(&|$)/)
-            || mw.config.get('wgUserName') === null
+            || mw.config.get('wgUserName') !== null
         ) {
             // Not MediaWiki / Api Document / Force Skin / Loginned
             return;
